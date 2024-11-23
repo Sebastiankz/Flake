847bee30b55f4df0f7bacfd9184650636a48bd7f
zharicks <128859733+zharicks@users.noreply.github.com>
2024-11-22 22:15:22 -0500

    Versión Final Frontend

---
+++ Frontend/src/pages/AdmNotas.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/admnotas.css";
+
+
+const Horario = () => {
+    return (
+        <div className="horario-container">
+            <h1 className="typewriter-title"> Gestión de notas </h1>
+        </div>
+    );
+};
+
+export default Horario;
